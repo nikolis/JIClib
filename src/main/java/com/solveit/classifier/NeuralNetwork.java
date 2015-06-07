d7ed7e59c76508c65e1ba9d0f75dc07b0bd70087
unknown <dep05156@DEPBBLHPS2NB0MA.code1.emi.philips.com>
2015-06-07 16:57:06 +0200

    Restructured the project in order to seperate the library of its use case

--- src/main/java/com/solveit/classifier/NeuralNetwork.java
+++
@@ -1,6 +0,0 @@
-package com.solveit.classifier;
-
-public interface NeuralNetwork {
-	public void loadInputs(final double x[][],final double y[][]) ; 
-	public double[][]  predict(double Xin[][]) ;
-}
